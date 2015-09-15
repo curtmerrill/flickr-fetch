fabed7aecbdd12e1048c2f2d3c16af3482dfe5ee
Curt Merrill <curt@curtmerrill.com>
2015-09-15 09:37:17 -0400

    a little bit of progress

--- fetch_photos.py
+++ fetch_photos.py
@@ -3,7 +3,9 @@ from fetch_config import *
 
 FLICKR_URL = "https://api.flickr.com/services/rest/"
 
-def get_photos():
+# This gets the album details, which includes a list of photos
+def get_album():
+
     api_options = {
         "method": "flickr.photosets.getPhotos",
         "api_key": FLICKR_API_KEY,
@@ -12,7 +14,25 @@ def get_photos():
         "nojsoncallback": "1",
     } 
 
-    photos = requests.get(FLICKR_URL, params=api_options).json()
-    return photos
+    album = requests.get(FLICKR_URL, params=api_options).json()
+    return album
 
-print(get_photos())
+print(get_album())
+
+# This loops through the list of photos fetched earlier and gets
+# more detail for each photo
+def get_photos():
+
+    # Requires 2 calls per photo:
+    # flickr.photos.getInfo for date, title, tags, etc.
+    # flickr.photos.getSizes for thumbnail/full-size img urls
+
+    api_options = {
+        # "method": "flickr.photosets.getPhotos",
+        "api_key": FLICKR_API_KEY,
+        "photoset_id": FLICKR_ALBUM_ID,
+        "format": "json",
+        "nojsoncallback": "1",
+    } 
+
+    pass
